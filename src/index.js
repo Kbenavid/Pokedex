40125be722b3efc55eb7e40ec4a0cd1c56cc6681
Kbenavid <kb3navides@gmail.com>
2025-09-18 05:29:21 -0700

    use app.jsx entry, clean CRA template

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+import App from "./App.jsx";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
