93905860c54fb983beac6fcda873c2871c33a635
Kbenavid <kb3navides@gmail.com>
2025-09-22 05:48:36 -0700

    Fix: default pokemon parameter tp avpid map on undefined

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Pokecard from './Pokecard';
 
-function Pokedex({ pokemon }) {
+const DEFAULT_POKEMON = [
+  { id: 4, name: 'Charmander', type: 'fire', base_experience: 62 },
+  { id: 7, name: 'Squirtle', type: 'water', base_experience: 63 },
+  { id: 11, name: 'Metapod', type: 'bug', base_experience: 72 },
+  { id: 12, name: 'Butterfree', type: 'flying', base_experience: 178 },
+  { id: 25, name: 'Pikachu', type: 'electric', base_experience: 112 },
+  { id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95 },
+  { id: 94, name: 'Gengar', type: 'poison', base_experience: 225 },
+  { id: 133, name: 'Eevee', type: 'normal', base_experience: 65 },
+];
+
+function Pokedex({ pokemon = DEFAULT_POKEMON }) {
   return (
     <div className="pokedex">
       {pokemon.map((p) => (
@@ -27,19 +38,6 @@ Pokedex.propTypes = {
       base_experience: PropTypes.number.isRequired,
     })
   ),
-};
-
-Pokedex.defaultProps = {
-  pokemon: [
-    { id: 4, name: 'Charmander', type: 'fire', base_experience: 62 },
-    { id: 7, name: 'Squirtle', type: 'water', base_experience: 63 },
-    { id: 11, name: 'Metapod', type: 'bug', base_experience: 72 },
-    { id: 12, name: 'Butterfree', type: 'flying', base_experience: 178 },
-    { id: 25, name: 'Pikachu', type: 'electric', base_experience: 112 },
-    { id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95 },
-    { id: 94, name: 'Gengar', type: 'poison', base_experience: 225 },
-    { id: 133, name: 'Eevee', type: 'normal', base_experience: 65 },
-  ],
 };
 
 export default Pokedex;
